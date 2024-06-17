732153cf6c4802e326271bbb3993d2e9cbc7bed2
Anurag2618g <anurag2618g@gmail.com>
2024-06-17 23:16:36 +0530

    add patch route

--- index.js
+++ index.js
@@ -67,6 +67,20 @@ app.post("/posts", (req,res) => {
 });
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch("/posts/:id", (req,res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === id);
+  const updated = {
+    id: id,
+    title: req.body.title || post.title,
+    content: req.body.content || post.content,
+    author: req.body.author || post.author,
+    date: new Date,
+  };
+  const index = posts.findIndex((q) => q.id === id);
+  posts[index] = updated;
+  res.json(updated);
+});
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
 app.delete("/posts/:id", (req,res) => {
